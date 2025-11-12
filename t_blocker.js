5607aeae91ec696dce58cbd4c4041ff1739eeb70
양소율(Rayna Yang) <98443232+soyulyang@users.noreply.github.com>
2025-11-12 15:08:29 +0900

    Delete t_blocker.js

--- t_blocker.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('hashchange', function() {
-    if (window.location.hash === '#google_vignette') {
-        var cleanUrl = window.location.href.split('#')[0];
-        window.location.replace(cleanUrl);
-    }
-});
